5e9ca7f3a51e42093ea0d82457f16629c5a7166c
Diego Barbo <diegobarbox@gmail.com>
2022-04-14 21:58:26 -0300

    serializers

--- config/settings.py
+++ config/settings.py
@@ -31,8 +31,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts',
+    'rest_framework',
+    'posts.apps.PostsConfig',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
